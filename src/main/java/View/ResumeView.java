fa4b50051a4fab062f1f2f04a8df35c964a280c8
tooli38 <tooli38@gmail.com>
2023-10-09 15:17:31 +0900

    ResumeView 클래스 설계
    사용자로부터 개인정보, 학력정보, 경력정보, 자기소개정보를 입력받을 수 있는 클래스를 설계하였다.

---
+++ src/main/java/View/ResumeView.java
@@ -0,0 +1,42 @@
+package View;
+
+import Model.Career;
+import Model.Education;
+import Model.PersonInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ResumeView {
+
+    private Scanner sc;
+
+    public ResumeView() {
+        sc = new Scanner(System.in);
+    }
+
+    public PersonInfo inputPersonInfo() {
+        PersonInfo personInfo = PersonInfo.of("test","test","test","test","test","test");
+
+        return personInfo;
+    }
+
+    public List<Education> inputEducationList() {
+        List<Education> educations = List.of();
+
+        return educations;
+    }
+
+    public List<Career> inputCareerList() {
+        List<Career> careers = List.of();
+
+        return careers;
+    }
+
+    public String inputSelfIntroduction() {
+        String selfIntroduction = sc.nextLine();
+
+        return selfIntroduction;
+    }
+}
