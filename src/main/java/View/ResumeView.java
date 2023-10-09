55c66148431f19fb7ea81d26883bcab91c1a64c2
tooli38 <tooli38@gmail.com>
2023-10-09 15:41:32 +0900

    사용자 개인정보를 입력받는 메소드 기능 구현
    Scanner를 활용해서 사용자의 정보를 입력받고 PersonInfo 객체를 반환하는 메소르드를 구현하였다.

--- src/main/java/View/ResumeView.java
+++ src/main/java/View/ResumeView.java
@@ -3,13 +3,10 @@ package View;
 import Model.Career;
 import Model.Education;
 import Model.PersonInfo;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ResumeView {
-
     private final Scanner sc;
 
     public ResumeView() {
@@ -17,7 +14,20 @@ public class ResumeView {
     }
 
     public PersonInfo inputPersonInfo() {
-        PersonInfo personInfo = PersonInfo.of("test","test","test","test","test","test");
+        System.out.print("사진 파일명을 입력하세요:");
+        String photo = sc.nextLine();
+        System.out.print("이름을 입력하세요:");
+        String name = sc.nextLine();
+        System.out.print("이메일을 입력하세요:");
+        String email = sc.nextLine();
+        System.out.print("주소를 입력하세요:");
+        String address = sc.nextLine();
+        System.out.print("전화번호를 입력하세요:");
+        String phoneNumber = sc.nextLine();
+        System.out.print("생년월일을 입력하세요 (예: 1990-01-01):");
+        String birthDate = sc.nextLine();
+
+        PersonInfo personInfo = PersonInfo.of(photo, name, email, address, phoneNumber, birthDate);
 
         return personInfo;
     }
